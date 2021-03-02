258566cb6a2e3a5135288186fb69002736830d81
Noah (FeaturedSpace) <noahcoder77@gmail.com>
2021-03-02 08:23:06 -0500

    Update Documentation for 1.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Noah Coetsee'
 author = 'Noah Coetsee'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
