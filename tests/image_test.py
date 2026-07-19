1982f0cb78c7c11c3765dce5280cda38d9ceb698
Noah Coetsee <noahcoder77@gmail.com>
2026-07-19 12:13:23 -0400

    Fix image path issue in image test + few other small changes

--- tests/image_test.py
+++ tests/image_test.py
@@ -2,8 +2,11 @@
 Image Test: Tests methods in the Image class
 """
 
+import os
 import unittest
 from pydraw import *
+
+IMAGE_PATH = os.path.join(os.path.dirname(__file__), '..', 'images', 'cool_barry.jpg')
 
 
 class ImageTest(unittest.TestCase):
@@ -13,14 +16,21 @@ class ImageTest(unittest.TestCase):
         cls.screen = Screen(800, 600)
 
     def test_creation(self):
-        image = Image(self.screen, '../cool_barry.jpg', screen.width() / 2, screen.height() / 2, 50, 50,
+        image = Image(self.screen, IMAGE_PATH, self.screen.width() / 2, self.screen.height() / 2, 50, 50,
                       color=Color('magenta'), border=Color('green'), rotation=30)
+
+        self.assertEqual(image.width(), 50)
+        self.assertEqual(image.height(), 50)
 
         self.screen.clear()
 
     def test_rotation(self):
-        image = Image(self.screen, '../cool_barry.jpg', screen.width() / 2, screen.height() / 2, 50, 50)
+        image = Image(self.screen, IMAGE_PATH, self.screen.width() / 2, self.screen.height() / 2, 50, 50)
         image.rotation(30)
+
+        self.assertEqual(image.rotation(), 30)
+
+        self.screen.clear()
 
 
 if __name__ == '__main__':
